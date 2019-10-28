2849de8f2f106c8396e0cf64286eea2c62e4091c
Audrey Guiongo <audreymorganedto@gmail.com>
2019-10-28 19:56:01 +0100

    doing exercice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import QuoteCitation from "./Components/QuoteCitation";
+
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    simpson: null
+  };
+
+  componentDidMount() {
+    this.getSimpson();
+  }
+
+  getSimpson = this.getSimpson.bind(this);
+
+  getSimpson() {
+    axios
+      .get("https://quests.wilders.dev/simpsons-quotes/quotes")
+      .then(response => response.data)
+      .then(data => {
+        this.setState({
+          simpson: data[0]
+        });
+      });
+  }
+
+  render() {
+    const { simpson } = this.state;
+    return (
+      <div>
+        {this.state.simpson ? (
+          <QuoteCitation simpson={this.state.simpson} />
+        ) : (
+          <p>no data</p>
+        )}
+        <button type="button" onClick={this.getSimpson}>
+          Get new simpson 
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
